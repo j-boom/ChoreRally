269c0c7d8dcc4988f823d0692fed95f53db3ef6c
Jim Bergren <jim.bergren@gmail.com>
2025-08-23 14:38:55 -0400

    Initial Commit

---
+++ ChoreRally/ChoreRallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreRallyApp.swift
+//  ChoreRally
+//
+//  Created by Jim Bergren on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ChoreRallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoreRallyTests/ChoreRallyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreRallyTests.swift
+//  ChoreRallyTests
+//
+//  Created by Jim Bergren on 8/23/25.
+//
+
+import Testing
+@testable import ChoreRally
+
+struct ChoreRallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
